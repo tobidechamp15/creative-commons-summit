df4cdb8d9a458bb426b9ad345602ca4fc02729b5
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2023-08-09 17:57:30 +0100

    about page

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const sideBar = document.querySelector(".side-bar");
 const menuBar = document.querySelector(".menu-bar");
 const closeBtn = document.querySelector(".close");
 
+
 menuBar.addEventListener("click", () => {
   console.log("object");
   sideBar.classList.toggle("side-barVisible");
