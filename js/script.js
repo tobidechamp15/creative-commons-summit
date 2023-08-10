8b127985a247296d4aca0fdc2e60c72024c58133
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2023-08-10 17:11:11 +0100

    main program

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const sideBar = document.querySelector(".side-bar");
 const menuBar = document.querySelector(".menu-bar");
 const closeBtn = document.querySelector(".close");
-
+const secondSection = document.querySelector(".speakerSecSect");
+const viewMore = document.querySelector(".view-more");
+const viewLess = document.querySelector(".view-less");
+secondSection.style.display="none"
 
 menuBar.addEventListener("click", () => {
   console.log("object");
@@ -9,5 +12,17 @@ menuBar.addEventListener("click", () => {
 });
 
 closeBtn.addEventListener("click", () => {
-    sideBar.classList.remove("side-barVisible");
+  sideBar.classList.remove("side-barVisible");
+});
+viewMore.style.display = "flex";
+viewMore.addEventListener("click", () => {
+  secondSection.style.display = "flex";
+  viewMore.style.display="none";
+  viewLess.style.display="flex";
+});
+
+viewLess.addEventListener("click", () => {
+  secondSection.style.display="none";
+  viewMore.style.display = "flex";
+  viewLess.style.display="none";
 })
