34f75c9a2e61557ea8b05619bd35009b4b91c222
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2023-08-23 14:11:42 +0100

    final-touches

--- js/about.js
+++ js/about.js
@@ -2,18 +2,13 @@ const toProgram = document.querySelector(".toProgram");
 toProgram.addEventListener("click", function () {
   var section = document.querySelector(".mainProgram");
   section.scrollIntoView({ behavior: "smooth" });
-  console.log("clikkk");
+  console.log("click");
 });
 
 const toAbout = document.querySelector(".toAbout");
 toAbout.addEventListener("click", function () {
   var section = document.querySelector(".name-logo");
   section.scrollIntoView({ behavior: "smooth" });
-  console.log("clikkk");
+  console.log("click");
 });
 
-// Add animation to section .toAbout
-var aboutSection = document.getElementById("aboutMe"); // Get the element with id="demo".
-window.onscroll = function () {
-  myFunction();
-}; // Run every time you scroll and check if it's in view or not, then
